8b531690eb07849d1f7263a1fa1d130cedf44fac
Soumo Sarkar <soumosarkar297@gmail.com>
2025-08-29 22:13:24 +0530

    Fix distance calculation to correctly account for z-coordinates
    `distance()` function has a bug:
    It was wrong, because p1.z, p2.z were not subtracted.

--- Automation/main.js
+++ Automation/main.js
@@ -148,5 +148,5 @@ function drawOutput(distanceToBalls) {
 
 
 function distance(p1, p2) {
-  return Math.hypot(p1.x - p2.x, p1.y - p2.y, p1.z, p2.z);
+  return Math.hypot(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
 }
