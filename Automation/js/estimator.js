8e0fe31f267fcdc24cb762661a9f0ebda66a30a1
Soumo Sarkar <soumosarkar297@gmail.com>
2025-08-30 08:16:01 +0530

    Refactor the code and separate out functions & variables in different `.js` files

---
+++ Automation/js/estimator.js
@@ -0,0 +1,32 @@
+let initialApparentRadii = null;
+
+function estimateDistancesToBalls(rgbCounts) {
+  const apparentRadii = rgbCounts.map((area) => Math.sqrt(area / Math.PI));
+
+  if (!initialApparentRadii) {
+    initialApparentRadii = apparentRadii;
+  }
+
+  const distancesToBalls = [];
+
+  for (let i = 0; i < apparentRadii.length; i++) {
+    const ratio = initialApparentRadii[i] / apparentRadii[i];
+    distancesToBalls[i] = INITIAL_DISTANCES_TO_BALLS[i] * ratio;
+  }
+
+  return distancesToBalls;
+}
+
+function estimateCameraPosition(distancesToBalls) {
+  const d = DISTANCE_BETWEEN_BALLS;
+
+  const a = distancesToBalls[0];
+  const b = distancesToBalls[1];
+  const c = distancesToBalls[2];
+
+  const x = (c ** 2 - d ** 2 - b ** 2) / (2 * d);
+  const z = Math.sqrt(b ** 2 - x ** 2);
+
+  const xOffset = d / 2;
+  return { x: x + xOffset, z };
+}
---
+++ Automation/js/main.js
@@ -0,0 +1,34 @@
+const cameraPositions = [];
+
+fileInput.onchange = function (event) {
+  const file = event.target.files[0];
+  inputVideo.src = URL.createObjectURL(file);
+  inputVideo.play();
+};
+
+inputVideo.onloadeddata = function () {
+  inputCanvas.width = inputVideo.videoWidth;
+  inputCanvas.height = inputVideo.videoHeight;
+  segmentationCanvas.width = inputVideo.videoWidth;
+  segmentationCanvas.height = inputVideo.videoHeight;
+  loop();
+};
+
+function loop() {
+  const { width, height } = inputCanvas;
+  const ctx = inputCanvas.getContext("2d", { willReadFrequently: true });
+  ctx.drawImage(inputVideo, 0, 0);
+
+  const imgData = ctx.getImageData(0, 0, width, height);
+
+  const rgbCounts = segmentImage(imgData);
+
+  const distancesToBalls = estimateDistancesToBalls(rgbCounts);
+
+  const cameraPosition = estimateCameraPosition(distancesToBalls);
+  cameraPositions.push(cameraPosition);
+
+  drawOutput(distancesToBalls, cameraPositions);
+
+  requestAnimationFrame(loop);
+}
